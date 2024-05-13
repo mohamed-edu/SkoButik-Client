7e44524ecccbb71f952e29a3348caad88873ddb1
Olov <olovolsson@live.se>
2024-05-13 11:23:36 +0200

    fixed some errorrs

--- Areas/Identity/Pages/Account/Register.cshtml.cs
+++ Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -82,16 +82,6 @@ namespace SkoButik_Client.Areas.Identity.Pages.Account
         /// </summary>
         public class InputModel
         {
-
-            [Required]
-            [StringLength(50, MinimumLength = 2, ErrorMessage = "First name must be between 2 and 50 characters")]
-            [Display(Name = "First Name")]
-            public string FirstName { get; set; }
-
-            [Required]
-            [StringLength(50, MinimumLength = 2, ErrorMessage = "Last name must be between 2 and 50 characters")]
-            [Display(Name = "Last Name")]
-            public string LastName { get; set; }
             /// <summary>
             ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
             ///     directly from your code. This API may change or be removed in future releases.
@@ -100,17 +90,6 @@ namespace SkoButik_Client.Areas.Identity.Pages.Account
             [EmailAddress]
             [Display(Name = "Email")]
             public string Email { get; set; }
-
-            [Required]
-            [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 2)]
-            [Display(Name = "Address")]
-            public string Address { get; set; }
-
-            [Required]
-            [DataType(DataType.PhoneNumber)]
-            [Display(Name = "Phone Number")]
-            public string Phone { get; set; }
-
 
             /// <summary>
             ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
