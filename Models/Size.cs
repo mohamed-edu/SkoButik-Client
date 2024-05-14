c713eb8eac0d1064c1aa23bc9fd3d6399b9fb8ee
TrainingSpace <Johanna.marklund@edu.edugrade.se>
2024-05-14 11:40:36 +0200

    Add and connected Product, brand and size

---
+++ Models/Size.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace SkoButik_Client.Models
+{
+    public class Size
+    {
+        [Key]
+        public int SizeId { get; set; }
+
+        [Required]
+        [StringLength(20, MinimumLength = 10)]
+        [DisplayName("Size")]
+        public string SizeName {  get; set; }
+
+        public ICollection<Product> Products { get; set;}
+    }
+}
