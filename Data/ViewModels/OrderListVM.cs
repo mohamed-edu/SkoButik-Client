8cde7950d06e17909a713509f82c1de96458123d
Olov <olovolsson@live.se>
2024-05-17 15:14:54 +0200

    Added admin functions.

---
+++ Data/ViewModels/OrderListVM.cs
@@ -0,0 +1,9 @@
+﻿namespace SkoButik_Client.Models
+{
+    public class OrderListViewModel
+    {
+        public List<Order> Orders { get; set; }
+        public string SelectedCurrency { get; set; }
+        public decimal ExchangeRate { get; set; }
+    }
+}
