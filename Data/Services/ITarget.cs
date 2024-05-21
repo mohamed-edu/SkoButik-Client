6a4eeac6cf99632ebc63a732fe8948ea0df1616c
Johanna <johannanmarklund@gmail.com>
2024-05-21 11:27:53 +0200

    Updated dashboardstats with campaignprices

---
+++ Data/Services/ITarget.cs
@@ -0,0 +1,9 @@
+﻿using SkoButik_Client.Models;
+
+namespace SkoButik_Client.Data.Services
+{
+    public interface ITarget
+    {
+        decimal CalculateTotalSales(List<OrderItem> orderItems);
+    }
+}
