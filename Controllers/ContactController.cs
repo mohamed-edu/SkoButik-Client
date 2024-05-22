56b88bf3cee37c20de67e7b34b3d147aecbe11c8
PeterMolen <144238673+PeterMolen@users.noreply.github.com>
2024-05-22 10:11:13 +0200

    Created Contact, and changed some css

---
+++ Controllers/ContactController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SkoButik_Client.Models;
+using static SkoButik_Client.Models.Contact;
+
+namespace SkoButik_Client.Controllers
+{
+    public class ContactController : Controller
+    {
+        // GET: Contact
+        public ActionResult Contact()
+        {
+            var random = new Random();
+            var phone = $"555-01{random.Next(0, 10)}-{random.Next(1000, 10000)}";
+            var email = $"contact{random.Next(1, 100)}@G5.com";
+
+            var model = new Contact
+            {
+                Phone = phone,
+                Email = email
+            };
+
+            return View(model);
+        }
+    }
+}
