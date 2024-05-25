cdcd0e9d0d75ec038452f61e3dde64dea661cd0a
mohamed-edu <mohamed.mohamud3222@gmail.com>
2024-05-25 13:43:44 +0200

    fixed admin dashboard/pages

---
+++ Data/ViewModels/AdminDashboardViewModel.cs
@@ -0,0 +1,12 @@
+﻿using SkoButik_Client.Models;
+
+namespace SkoButik_Client.Data.ViewModels
+{
+    public class AdminDashboardViewModel
+    {
+        public List<OrderStatsViewModel> OrderStats { get; set; }
+        public ProductStatsViewModel MostSoldProduct { get; set; }
+        public List<Order> Orders { get; set; }
+        public List<Inventory> Inventories { get; set; }
+    }
+}
